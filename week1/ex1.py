9b051bd62fb96c0f1eabed4d6c9577d34675ec5a
Yuvesh <yuvesh1012@gmail.com>
2025-12-24 00:12:14 +0530

    DSA

---
+++ week1/ex1.py
@@ -0,0 +1,19 @@
+#Program on Creating and Getting variables of different data types
+
+#Age using int
+Age=int(input("Enter Your Age:"))
+
+#Height using float
+Height=float(input("Enter your height in cm:"))
+
+#Name using string
+Name=input("Enter your name:")
+
+#Student? using boolean
+Student=bool(input("Are you a student (True/False):"))
+
+#Output
+print("Your Age:",Age)
+print("Your Height:",Height)
+print("Your Name:",Name)
+print("Are you a student?:",Student)
