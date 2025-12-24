4a48b0e23206f62a59c70b55da2f67697567fb7c
Yuvesh <yuvesh1012@gmail.com>
2025-12-25 01:37:23 +0530

    Determining Integer

---
+++ week1/ex7.py
@@ -0,0 +1,12 @@
+#Program on determining integer
+
+#Input
+integer=int(input("Enter Your Integer:"))
+
+#Output
+if integer>0:
+    print("Its a Positive Number")
+elif integer==0:
+    print("Its Zero")
+else:
+    print("Its a Negative Number")
