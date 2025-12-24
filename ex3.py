c196423b9e3e6d3b9ff63d51c62ad356231a2b2a
Yuvesh <yuvesh1012@gmail.com>
2025-12-24 22:51:32 +0530

    Discounted Price

---
+++ ex3.py
@@ -0,0 +1,10 @@
+#Calculating Final Price
+
+#Assigning the values for variables
+price=599.99
+discount=0.15
+discountedprice=price*discount
+
+#Output
+finalprice=price-discountedprice
+print("Price After the discount is:%.2f"%finalprice) #%.2f rounds off the value into two decimal points
