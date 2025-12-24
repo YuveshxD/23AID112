d1835004d6e578d3df40b59ff2ec68570eb4cb21
Yuvesh <yuvesh1012@gmail.com>
2025-12-24 22:38:49 +0530

    AGE CALCULATOR(2025)

---
+++ week1/ex2.py
@@ -0,0 +1,9 @@
+#Program on Calculating Your Age in 2025
+
+#Input
+BirthYear=input("Enter Your Birth Year:")
+Year=int(BirthYear) #string into int
+Age=2025-Year
+
+#Output
+print("Your age in 2025 is:",Age)
