750c08ce28e9f58bff788b2a4f478fc990bbc219
Yuvesh <yuvesh1012@gmail.com>
2025-12-26 22:33:37 +0530

    Update ex10.py

--- week1/ex10.py
+++ week1/ex10.py
@@ -8,13 +8,11 @@ fahrenheit=celsius*(9/5)+32
 
 #Output
 print("Temperature in fahrenheit scale:",fahrenheit)
-if fahrenheit<32:                  #0degree in celsius is 32degree in fahrenheit
+if fahrenheit<32:                 
     print("Very cold! Wear thick jacket")
-elif 32<=fahrenheit<=59:           #15degree in celsius is 59degree in fahrenhei
+elif 32<=fahrenheit<=59:           
     print("Cold. Wear jacket")
 elif 60<=fahrenheit<=77: 
     print("Nice weather")
 else:
     print("Hot! Stay hydrated")
-
-
